95d86a6f1465142df0c2f3c576c0e4207048f560
Oleksandr Tkachenko <o.tkachenko.ukr@gmail.com>
2019-08-04 22:09:30 +0300

    Examination: Add SE polices for LedControl HAL
    Signed-off-by: Oleksandr Tkachenko <o.tkachenko.ukr@gmail.com>

--- Android_examination_work/hal/config/hikey960.mk
+++ Android_examination_work/hal/config/hikey960.mk
@@ -2,3 +2,4 @@ PRODUCT_PACKAGES += vendor.gl.ledcontrol@1.0-service.hikey960
 TARGET_FS_CONFIG_GEN += vendor/gl/hal/config/config.fs
 DEVICE_MANIFEST_FILE += vendor/gl/hal/interfaces/manifest.xml
 DEVICE_MATRIX_FILE += vendor/gl/hal/interfaces/compatibility_matrix.xml
+BOARD_SEPOLICY_DIRS += vendor/gl/hal/sepolicy
